c07772ce6bba3586dd52e99a5c053ed8c27569e3
Aryan Sharma <asharma00@stuy.edu>
2019-03-21 09:54:25 -0400

    constructors

--- MyDeque.java
+++ MyDeque.java
@@ -2,14 +2,40 @@ public class MyDeque<E>{
   private E[] data;
   private int size, start, end;
 
-  public MyDeque(){  }
-  public MyDeque(int initialCapacity){  }
-  public int size(){ }
-  public String toString(){ }
-  public void addFirst(E element){ }
-  public void addLast(E element){ }
-  public E removeFirst(E element){ }
-  public E removeLast(E element){ }
-  public E getFirst(E element){ }
-  public E getLast(E element){ }
+  public MyDeque(){
+    @SuppressWarnings("unchecked")
+    E[] d = (E[])new Object[10];
+    data = d;
+  }
+
+  public MyDeque(int initialCapacity){
+    @SuppressWarnings("unchecked")
+    E[] d = (E[])new Object[initialCapacity];
+    data = d;
+  }
+  
+  public int size(){
+
+  }
+  public String toString(){
+
+   }
+  public void addFirst(E element){
+
+   }
+  public void addLast(E element){
+
+   }
+  public E removeFirst(E element){
+
+   }
+  public E removeLast(E element){
+
+  }
+  public E getFirst(E element){
+
+  }
+  public E getLast(E element){
+
+  }
 }
