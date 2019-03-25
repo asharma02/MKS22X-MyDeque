cab4bfeec8076c7790c1fd317f199524be411356
Aryan Sharma <asharma00@stuy.edu>
2019-03-25 10:08:06 -0400

    sepertated values/operators

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,17 @@
+import java.util.Arrays;
+
 public class Calculator{
     /*Evaluate a postfix expression stored in s.
      *Assume valid postfix notation, separated by spaces.
      */
-    public static double eval(String s){}
+    public static double eval(String s){
+      String[] vals = s.split(" ");
+      System.out.println(Arrays.toString(vals));
+      return 0.0;
+    }
+
+public static void main(String[] args) {
+  System.out.println(eval("8 2 + 99 9 - * 2 + 9 -"));
+}
+
 }
