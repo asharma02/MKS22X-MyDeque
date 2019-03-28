cd36b9d0e473f87cb9a83abcc3c08f4af1fa9a24
Aryan Sharma <asharma00@stuy.edu>
2019-03-27 23:02:08 -0400

    finished calc, fixed errors, used GG driver

--- Calculator.java
+++ Calculator.java
@@ -9,38 +9,37 @@ public class Calculator{
       //System.out.println(Arrays.toString(vals));
       MyDeque<Double> expression = new MyDeque<Double>();
       for (int i = 0; i < vals.length; i++) { //loop through array
-        if(vals[i].equals("+")){
-                  Double firstval = expression.removeLast();
-                  Double secval = expression.removeLast();
-                  expression.addLast(secval + firstval);
-                }
-                else if(vals[i].equals("-")){
-                  Double firstval = expression.removeLast();
-                  Double secval = expression.removeLast();
-                  expression.addLast(secval - firstval);
-                }
-                else if(vals[i].equals("*")){
-                  Double firstval = expression.removeLast();
-                  Double secval = expression.removeLast();
-                  expression.addLast(secval * firstval);
-                }
-                else if(vals[i].equals("/")){
-                  Double firstval = expression.removeLast();
-                  Double secval = expression.removeLast();
-                  expression.addLast(secval / firstval);
-                }
-                else if(vals[i].equals("%")){
-                  Double firstval = expression.removeLast();
-                  Double secval = expression.removeLast();
-                  expression.addLast(secval % firstval);
-                }
-                else{
-                  //if the value is not an operation
-                  Double x = Double.parseDouble(vals[i]);
-                  expression.addLast(x);
-                }
-              }
-      return expression.getLast();
+        if(vals[i].equals("+")){ //check if equal to operator
+          Double firstval = expression.removeLast(); //pop the last one (top)
+          Double secval = expression.removeLast(); //pop the second to last one; so get the last two
+          expression.addLast(secval + firstval); //do operation and add it back in
+        }
+        else if(vals[i].equals("-")){
+          Double firstval = expression.removeLast();
+          Double secval = expression.removeLast();
+          expression.addLast(secval - firstval);
+        }
+        else if(vals[i].equals("*")){
+          Double firstval = expression.removeLast();
+          Double secval = expression.removeLast();
+          expression.addLast(secval * firstval);
+        }
+        else if(vals[i].equals("/")){
+          Double firstval = expression.removeLast();
+          Double secval = expression.removeLast();
+          expression.addLast(secval / firstval);
+        }
+        else if(vals[i].equals("%")){
+          Double firstval = expression.removeLast();
+          Double secval = expression.removeLast();
+          expression.addLast(secval % firstval);
+        }
+        else{ //if it is a number and not an operator, turn into double and insert back in
+          Double x = Double.parseDouble(vals[i]);
+          expression.addLast(x);
+        }
+      }
+      return expression.getLast();//return the last num
 
     }
 
